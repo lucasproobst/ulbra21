04a0b69eb07a80d3c0f98ba003bc5775788bbb42
lucasproobst <lucasprobst@rede.ulbra.br>
2021-04-22 18:23:12 -0300

    primeiro commit

---
+++ 2021-1/IAC/instaAutomacao.py
@@ -0,0 +1,48 @@
+Descrição
+
+A pulseira inteligente é ideal para quem deseja ter ao alcance de suas mãos informações como:
+
+Notificação de aplicativos: WhatsApp, WeChat, QQ, Twitter, Facebook
+Taxa
+Pressão arterial
+Quantos passos deu o dia
+Quantas milhas caminhou
+Calorias que perderam
+Os links recebidos
+Despertador
+Reminder sedentário
+
+O relógio possui uma bateria de grande durabilidade.
+
+É resistente à água, podendo lavar as mãos ou até mesmo tomar banho. Proteção contra nível de água: IP67.
+
+Benefícios
+Bluetooth 4.0
+Tela sensível ao toque
+Sistema operacional compatível com Android 4.4 ou superior / iOS 8.0 ou superior
+Unissex
+
+ Adequado para
+Que desejam uma vida mais saudável ou que busquem praticidade no dia a dia.
+
+Cores
+Preto
+
+Qual será a caixa:
+1 relógio inteligente M3
+1x pulseira de silicone preta
+1 manual em chinês e inglês
+1x cabo USB
+
+Modo de usar
+Baixe as aplicações de deusas: Yoho Sports, background, wearfit, entre outras disponíveis e sincronize com seu relógio via Bluetooth.
+
+Características principais:
+Carregamento via USB;
+Capacidade da bateria: 85mAh
+Tempo de carregamento: cerca de 2 horas
+
+ 
+
+Dimensões
+Peso do produto: 0,0220 kg
